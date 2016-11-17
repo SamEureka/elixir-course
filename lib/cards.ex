568c3ce11c996cfdfeabeb9881f8386a76feb15f
SamEureka <sameureka@me.com>
2016-11-17 08:27:02 -0800

    done

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,21 +1,55 @@
 defmodule Cards do
+  @moduledoc """
+  This is awesome!
+  """
+
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    for suit <- suits do
-      suit
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
     end
 
   end
-
-
 
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Checks to make sure card exists in a generated deck.
+
+  ## Examples
+
+    iex> deck = Cards.create_deck
+    iex> Cards.contains?(deck, "King of Diamonds")
+    true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  def deal(deck, count) do
+    Enum.split(deck, count)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, reason} -> "Nope... #{reason}"
+    end
+  end
+
+  def create_hand(count) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(count)
   end
 end
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,14 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "create_deck makes 52 cards" do
+    deck_len = length(Cards.create_deck)
+    assert deck_len == 52
   end
+
+  test "shuffle randomizes the deck" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
+  end
+
 end
